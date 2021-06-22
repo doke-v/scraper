d1040be4b28b2b8a41fe56390fbf3823d3e1fc53
tuhnik <huntlarsen@gmail.com>
2021-06-22 22:11:49 +0300

    show changes in local time

--- scripts/updateReadme.js
+++ scripts/updateReadme.js
@@ -1,9 +1,11 @@
 const { writeFile } = require("fs/promises");
+const { DateTime } = require("luxon");
 const filePath = `${process.cwd()}/README.md`;
 
 const updateReadme = async (collectionSize) => {
+  const date = DateTime.now().setZone("Europe/Tallinn");
   const content = `Current collection size: **${collectionSize}**  
-Updated at: *${new Date()}*`;
+Updated at: *${date.toLocaleString(DateTime.DATETIME_MED)}*`;
 
   await writeFile(filePath, content, {
     encoding: "utf8",
