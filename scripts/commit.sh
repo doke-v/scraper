55ec93e5c35eaf2ca301591dba342e5f71301f7d
tuhnik <huntlarsen@gmail.com>
2021-06-22 20:36:24 +0300

    edit commit script

--- scripts/commit.sh
+++ scripts/commit.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-git add db/data.json
-git commit -m "update data"
-git add README.md
-git commit -m "update readme"
-git push origin main
+if git diff --exit-code --quiet db/data.json
+then
+    echo "no changes..."
+else
+    git add db/data.json
+    git commit -m "update data"
+    git add README.md
+    git commit -m "update readme"
+    git push origin main
+fi
